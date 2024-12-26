9a72794c625996fe45a9b4f93f060a85e9967a53
majmal08 <ajmalg08@gmail.com>
2024-12-26 15:27:08 +0500

    fix middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,15 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+
         if (! $request->expectsJson()) {
-            return route('login');
+            if (! $request->session()->has('user')) {
+                return route('login');
+            }
+            return route('home');
         }
+        return null;
     }
+
+
 }
